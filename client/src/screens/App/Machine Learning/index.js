4efac965eff6b4a69dde850e37165a8d1f95a35c
Denis Kison <denkison@gmail.com>
2024-02-16 16:01:06 +0100

    Disabled call to the ML server

--- client/src/screens/App/Machine Learning/index.js
+++ client/src/screens/App/Machine Learning/index.js
@@ -22,7 +22,7 @@ const ButtonWrapper = styled.div`
 const mlServerUrl = 'http://127.0.0.1:5000/machine-learning';
 
 const MachineLearning = () => {
-  const [prediction, setPrediction] = useState();
+  const [prediction, setPrediction] = useState(0);
   const location = useRouter();
   const currentPath = location.asPath;
 
@@ -53,103 +53,106 @@ const MachineLearning = () => {
       value13: event.target.renovate.value
     });
 
-    let result = await axios.post(mlServerUrl, data, headers)
+    /*let result = await axios.post(mlServerUrl, data, headers)
       .catch((err) =>
         console.log(err)
-      );
+      );*/
 
     intemptEventCreatedMlTask(currentPath)
 
-    setPrediction(result?.data?.prediction[0]);
+    //setPrediction(result?.data?.prediction[0]);
+    setPrediction(200000);
   };
 
   return (
     <Card>
       <h2>Fill out the form below to get a ML house price prediction</h2>
-      <h3>Your Predicted house price is: {prediction}</h3>
       <form onSubmit={mlAnalysis}>
         <InputWrapper>
           <FieldLabel>
             Bedrooms
-            <TextInput name="bedrooms" type="number" />
+            <TextInput name="bedrooms" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Bathrooms
-            <TextInput name="bathrooms" type="number" />
+            <TextInput name="bathrooms" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Square Foot Living, number around 2000
-            <TextInput name="sqlive" type="number" />
+            <TextInput name="sqlive" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Square Foot Lot, number around 15,000
-            <TextInput name="sqlot" type="number" />
+            <TextInput name="sqlot" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Number of floors
-            <TextInput name="floors" type="number" />
+            <TextInput name="floors" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Water front, 0 or 1
-            <TextInput name="water" type="number" />
+            <TextInput name="water" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Number of views, 0 to 4
-            <TextInput name="views" type="number" />
+            <TextInput name="views" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Condition, number from 1 to 5
-            <TextInput name="condition" type="number" />
+            <TextInput name="condition" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Grade, number from 1 to 13
-            <TextInput name="grade" type="number" />
+            <TextInput name="grade" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Square foot above, number around 1800
-            <TextInput name="sqabove" type="number" />
+            <TextInput name="sqabove" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Square foot basement, number around 300
-            <TextInput name="basement" type="number" />
+            <TextInput name="basement" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Year built, year around 1970
-            <TextInput name="built" type="number" />
+            <TextInput name="built" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <InputWrapper>
           <FieldLabel>
             Year renovated, year around 1980
-            <TextInput name="renovate" type="number" />
+            <TextInput name="renovate" type="number"/>
           </FieldLabel>
         </InputWrapper>
         <ButtonWrapper>
           <Button type="submit">Predict</Button>
         </ButtonWrapper>
       </form>
+      {prediction !== 0 && (
+          <h3>Your Predicted house price is: {prediction}</h3>
+      )}
     </Card>
   );
 };
